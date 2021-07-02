ef6b028d291cf725996aeaa8bb268fd817cc0859
Truhlar Tomas <truhlar@rcas6008.rcf.bnl.gov>
2021-07-02 09:05:58 -0400

    first commit

---
+++ prepareFolders.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+touch ZDC_code/seenRuns/seenRuns_run21.truhlar.txt
+mkdir ZDC_code/data/run21.ZdcCalibration.truhlar
+mkdir ZDC_Calibration/run21.ZdcCalibration.truhlar
+mkdir /afs/rhic.bnl.gov/star/users/truhlar/WWW/run21.ZdcCalibration
